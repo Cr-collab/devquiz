d9318fc41010724c13d3bc9c2d0a89223bd704ac
Cr-collab <cristiccorrea@gmail.com>
2022-09-08 19:52:48 -0300

    criando efeito de gradiente na tela de carregamento.

--- lib/splash/spladh_page.dart
+++ lib/splash/spladh_page.dart
@@ -1,10 +1,15 @@
+import 'package:DevQuiz/core/app_gradients.dart';
 import 'package:flutter/material.dart';
 
 class SplahPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: AppGradients.linear
+        ) ,
+      ),
     );
   }
 }
