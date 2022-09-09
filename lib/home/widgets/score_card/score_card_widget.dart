2faeb7f91ee7ede78979c0ad8fa80d0d17bb7c0d
Cr-collab <cristiccorrea@gmail.com>
2022-09-09 13:27:33 -0300

    app bar criada

---
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -0,0 +1,53 @@
+import 'package:DevQuiz/core/app_colors.dart';
+import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/home/widgets/char/char_widget.dart';
+import 'package:flutter/material.dart';
+
+class ScoreCardWidget extends StatefulWidget {
+  @override
+  State<ScoreCardWidget> createState() => _ScoreCardWidgetState();
+}
+
+class _ScoreCardWidgetState extends State<ScoreCardWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 14, left: 20, right: 20,bottom: 0),
+      child: Container(
+        height: 136,
+        decoration: BoxDecoration(
+            color: AppColors.white, borderRadius: BorderRadius.circular(15)),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                width: 80,
+                height: 80,
+                child: CharWidget(),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Vamos começar',
+                      style: AppTextStyles.body,
+                    ),
+                    Text('Complete ops desafios e avance em conhecimentos')
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
