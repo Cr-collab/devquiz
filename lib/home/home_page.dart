a212ff6da41a33cb6dd17e78c286888aa1eb6ef2
Cr-collab <cristiccorrea@gmail.com>
2022-09-09 13:24:59 -0300

    pagina inicila

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -11,7 +11,6 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
-      body: Text('Hello world'),
     );
   }
 }
