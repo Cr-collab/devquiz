b59beb860692968878a850e50828cbecef4d5473
Cr-collab <cristiccorrea@gmail.com>
2022-09-11 13:48:34 -0300

    criando espaçamento no nosso card

--- lib/home/widgets/score_card/score_card_widget.dart
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -17,35 +17,38 @@ class _ScoreCardWidgetState extends State<ScoreCardWidget> {
         height: 136,
         decoration: BoxDecoration(
             color: AppColors.white, borderRadius: BorderRadius.circular(15)),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              flex: 1,
-              child: Container(
-                width: 80,
-                height: 80,
-                child: CharWidget(),
-              ),
-            ),
-            Expanded(
-              flex: 3,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 24),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Vamos começar',
-                      style: AppTextStyles.body,
-                    ),
-                    Text('Complete ops desafios e avance em conhecimentos')
-                  ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  width: 80,
+                  height: 80,
+                  child: CharWidget(),
                 ),
               ),
-            )
-          ],
+              Expanded(
+                flex: 3,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 24),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Vamos começar',
+                        style: AppTextStyles.body,
+                      ),
+                      Text('Complete ops desafios e avance em conhecimentos')
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
